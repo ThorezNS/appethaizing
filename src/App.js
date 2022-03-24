79763fb51619786c61ce40b8ed323eecfdda7eae
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-03-24 21:39:25 +0100

    navigation bar styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import styles from './GlobalStyles.module.css';
+import NavBar from './components/NavBar';
+
 function App() {
   return (
-    <div className="App">
+    <div className={styles}>
+      <NavBar />
       <h1>hello world</h1>
     </div>
   );
