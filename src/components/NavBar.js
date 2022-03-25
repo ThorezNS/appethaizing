1f79ffebc17785a29cee8300e9a1ef626af781cc
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-03-25 22:20:41 +0100

    logo and title styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,15 @@
 import styles from './NavBar.module.css';
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.navBarContainer}>
+      <div className={styles.searchBarWrapper}>
+        <img className={styles.logo} src={logo} alt="appethaizing logo" />
+        <h1 className={styles.title}>appethaizing</h1>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
