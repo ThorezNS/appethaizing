ce98cb8f35cfc791051752acedced8288e099457
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-03-26 11:09:29 +0100

    search bar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,13 @@ const NavBar = () => {
       <div className={styles.searchBarWrapper}>
         <img className={styles.logo} src={logo} alt="appethaizing logo" />
         <h1 className={styles.title}>appethaizing</h1>
+        <div className={styles.searchBar}>
+          <input
+            className={styles.searchBarInput}
+            type="text"
+            placeholder="Find a recipe"
+          />
+        </div>
       </div>
     </div>
   );
