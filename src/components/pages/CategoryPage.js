a0412b0f830767db52def2fbff29081504ac4c2f
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-04-14 21:54:55 +0200

    added the content of the CategoryPage component

--- src/components/pages/CategoryPage.js
+++ src/components/pages/CategoryPage.js
@@ -1,12 +1,24 @@
 import { useParams } from 'react-router-dom';
+import useFetch from '../../containers/useFetch';
+import TheFetchMessage from '../atoms/TheFetchMessage/TheFetchMessage';
 import CardsContainer from '../organisms/CardsContainer/CardsContainer';
+import RecipeCard from '../organisms/RecipeCard/RecipeCard';
 
 const CategoryPage = () => {
   const { id } = useParams();
+  const KEY = process.env.REACT_APP_API_KEY;
+  const type = id === 'entree' ? 'main course' : id;
+  const { response, isLoading, error } = useFetch(
+    `https://api.spoonacular.com/recipes/complexSearch?apiKey=${KEY}&type=${type}&number=100`
+  );
 
   return (
     <CardsContainer page={id}>
-      <div>{id}</div>
+      <TheFetchMessage isLoading={isLoading} error={error} />
+      {response &&
+        response.results.map((recipe) => {
+          return <RecipeCard key={recipe.id} recipe={recipe} />;
+        })}
     </CardsContainer>
   );
 };
